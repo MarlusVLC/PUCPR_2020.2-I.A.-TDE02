ca77a62ecf11e9fd23a680df4e77e8ed6206da3e
Marlus Vinicius <marlusvinicius2014@outlook.com>
2020-11-21 20:42:46 -0300

    HashMap usada para ligar os movimentos aos estados seguintes. Loopbreaker implementado com uma penalizacao por voltar atrás. Talvez precise ser mudado.

--- src/player_2020_1_Equipe01/BFSPacManPlayer.java
+++ src/player_2020_1_Equipe01/BFSPacManPlayer.java
@@ -14,89 +14,133 @@ import java.util.Random;
  */
 public class BFSPacManPlayer implements PacManPlayer, StateEvaluator {
 
-    int DEBUG_randomCounter = 0;
-  /**
-   * Chooses a Move for Pac-Man in the Game.
-   * 
-   * @param game
-   * @return a Move for Pac-Man
-   */
+    /**
+     * Chooses a Move for Pac-Man in the Game.
+     *
+     * @param game
+     * @return a Move for Pac-Man
+     */
 
-  Random rand = new Random();
+    Move lastMove = Move.NONE;
 
-  public Move chooseMove(Game game) {
-      //TODO Para evitar loop: Gerar um randInt que seleciona um movimento do LegalMoves
-      //TODO Para evitar loop: Executar uma booleana que detecta se o PacMan já executou um movimento reverso.
-      State s = game.getCurrentState();
+    int turnaroundPenalty = 0;
 
-      HashMap<State, Move> nextNodes = new HashMap<>();
+    float totalScore = 0;
 
-      for (Move m : game.getLegalPacManMoves()){
+    int depth = 1;
+
+
+
+
+
+    public Move chooseMove(Game game) {
+
+
+        State s = game.getCurrentState();
+
+        HashMap<State, Move> nextNodes = new HashMap<>();
+
+        for (Move m : game.getLegalPacManMoves()){
+            //Coloca os movimentos legais em um HashMap que liga o estado diretamente com o movimento.
+            //A instabilidade na ordem dos itens no HashMap adiciona um elemento orgânico de aleatoriedade.
             nextNodes.put(game.getNextState(game.getCurrentState(), m), m);
-      }
-
-      State bestNode = game.getNextState(game.getCurrentState(), Move.NONE);
-      Move bestMove = Move.UP;
-
-      for (Map.Entry node : nextNodes.entrySet()) {
-          State nextNode = (State) node.getKey();
-          if (evaluateState(nextNode) >= evaluateState(bestNode)) {
-              bestNode = nextNode;
-              bestMove = nextNodes.get(bestNode);
-          }
-      }
-
-      return bestMove;
-  }
+        }
 
 
 
-  /**
-   * Computes an estimate of the value of the State.
-   * @param s the State to evaluate.
-   * @return an estimate of the value of the State.
-   */
-  public double evaluateState(State state) {
-     double heuristic = 1;
+//        System.out.println("Estados seguintes: " + nextNodes.entrySet());
+//        for (Map.Entry node : nextNodes.entrySet()) {
+//            System.out.println("Pontuacao do estados seguintes: " + evaluateState((State) node.getKey()));
+//        }
 
-    //TESTE DE FUNCAO OBJETIVO
-    if (Game.isLosing(state))
-      heuristic += Double.NEGATIVE_INFINITY;
+        State bestNode = game.getNextState(game.getCurrentState(), Move.NONE);
+//        System.out.println("Estado atual: " + game.getCurrentState());
+//        System.out.println("Pontuacao do estado atual: " + evaluateState(bestNode));
+        Move bestMove = Move.NONE;
+
+//      System.out.println(bestMove);
+
+
+        for (Map.Entry node : nextNodes.entrySet()) {
+
+
+            State nextNode = (State) node.getKey();
+
+
+            double turnaroundPenalty = (lastMove == nextNodes.get(nextNode).getOpposite() ? -10.0 : 0.0);
+
+
+            if (evaluateState(nextNode) + turnaroundPenalty >= evaluateState(bestNode)) {  //Isso evita que o PacMan faca o mesmo movimento anterior sem considerar danos graves
+                bestNode = nextNode;
+                bestMove = nextNodes.get(bestNode);
+            }
+        }
+//        System.out.println("Movimento escolhido: " + bestMove);
+//        System.out.println(" ");
+
+        lastMove = bestMove;
+        return bestMove;
+    }
+
+
+
+    /**
+     * Computes an estimate of the value of the State.
+     * @param s the State to evaluate.
+     * @return an estimate of the value of the State.
+     */
+    public double evaluateState(State state) {
+        Location pacLOC = state.getPacManLocation();
+        List<Location> allGhostLoc = state.getGhostLocations();
+        LocationSet allDotLoc = state.getDotLocations();
+
+
+        double heuristic = 1;
+
+        //TESTE DE FUNCAO OBJETIVO
+        if (Game.isLosing(state))
+            heuristic += Double.NEGATIVE_INFINITY;
 //        heuristic += 10000;
-    else if (Game.isWinning(state))
-      heuristic +=  Double.POSITIVE_INFINITY;
+        else if (Game.isWinning(state))
+            heuristic +=  Double.POSITIVE_INFINITY;
 //        heuristic -= 10000;
-    //TESTE DE FUNCAO OBJETIVO
+        //TESTE DE FUNCAO OBJETIVO
 
 
 
 //
 //    //HEURÍSTICA 1: DISTÂNCIA ENTRE PACMAN E O FANTASMA MAIS PRÓXIMO
-     Location pacLOC = state.getPacManLocation();
-     List<Location> allGhostLoc = state.getGhostLocations();
-
-     heuristic += pacLOC.manhattanDistanceToClosest(pacLOC, allGhostLoc);
+        heuristic += pacLOC.manhattanDistanceToClosest(pacLOC, allGhostLoc);
 //    //HEURÍSTICA 1: DISTÂNCIA ENTRE PACMAN E O FANTASMA MAIS PRÓXIMO
 
 
 
-      //HEURÍSTICA 2: PONTOS EXISTENTES NO MAPA
-      LocationSet allDotLoc = state.getDotLocations();
-      heuristic -= allDotLoc.size();
-      //HEURÍSTICA 2: PONTOS EXISTENTES NO MAPA
+        //HEURÍSTICA 2: PONTOS EXISTENTES NO MAPA
+        heuristic -= allDotLoc.size();
+        //HEURÍSTICA 2: PONTOS EXISTENTES NO MAPA
 
 
-      //HEURÍSTICA 3: DISTÂNCIA ENTRE O PAMAN E O PONTO MAIS PRÓXIMO
-//      heuristic -= pacLOC.manhattanDistanceToClosest(pacLOC, allDotLoc);
-      //HEURÍSTICA 3: DISTÂNCIA ENTRE O PAMAN E O PONTO MAIS PRÓXIMO
+        //HEURÍSTICA 3: DISTÂNCIA ENTRE O PACMAN E O PONTO MAIS PRÓXIMO
+        heuristic -= pacLOC.manhattanDistanceToClosest(pacLOC, allDotLoc);
+        //HEURÍSTICA 3: DISTÂNCIA ENTRE O PACMAN E O PONTO MAIS PRÓXIMO
 
 
 
-      return heuristic;
+        return heuristic;
 
-  }
+    }
 
+//    private int breadth_first_search(Game game, State state, int depth) {
+//        State s = game.getCurrentState();
+//
+//        HashMap<State, Move> nextNodes = new HashMap<>();
+//
+//        for (Move m : game.getLegalPacManMoves()) {
+//            //Coloca os movimentos legais em um HashMap que liga o estado diretamente com o movimento.
+//            //A instabilidade na ordem dos itens no HashMap adiciona um elemento orgânico de aleatoriedade.
+//            nextNodes.put(game.getNextState(game.getCurrentState(), m), m);
+//        }
+//    }
 
-  private float
 
 }
--- src/player_2020_1_Equipe01/GreedyPacManPlayer.java
+++ src/player_2020_1_Equipe01/GreedyPacManPlayer.java
@@ -11,7 +11,7 @@ import java.util.*;
  */
 public class GreedyPacManPlayer implements PacManPlayer, StateEvaluator {
 
-    int DEBUG_randomCounter = 0;
+//    int DEBUG_randomCounter = 0;
   /**
    * Chooses a Move for Pac-Man in the Game.
    * 
@@ -19,7 +19,13 @@ public class GreedyPacManPlayer implements PacManPlayer, StateEvaluator {
    * @return a Move for Pac-Man
    */
 
-  Random rand = new Random();
+//  Random rand = new Random();
+  int oppositeMove_Counter = 0;
+  Move lastMove = Move.NONE;
+  int turnaroundPenalty = 0;
+
+
+
 
   public Move chooseMove(Game game) {
       //TODO Para evitar loop: Gerar um randInt que seleciona um movimento do LegalMoves
@@ -29,20 +35,44 @@ public class GreedyPacManPlayer implements PacManPlayer, StateEvaluator {
       HashMap<State, Move> nextNodes = new HashMap<>();
 
       for (Move m : game.getLegalPacManMoves()){
+          //Coloca os movimentos legais em um HashMap que liga o estado diretamente com o movimento.
+          //A instabilidade na ordem dos itens no HashMap adiciona um elemento orgânico de aleatoriedade.
             nextNodes.put(game.getNextState(game.getCurrentState(), m), m);
       }
 
+
+      System.out.println("Estados seguintes: " + nextNodes.entrySet());                                  //DEBUG
+      for (Map.Entry node : nextNodes.entrySet()) {
+          System.out.println("Pontuacao do estados seguintes: " + evaluateState((State) node.getKey())); //DEBUG
+      }
+
       State bestNode = game.getNextState(game.getCurrentState(), Move.NONE);
-      Move bestMove = Move.UP;
+      System.out.println("Estado atual: " + game.getCurrentState());                                     //DEBUG
+      System.out.println("Pontuacao do estado atual: " + evaluateState(bestNode));                       //DEBUG
+      Move bestMove = Move.NONE;
+
+//      System.out.println(bestMove);
+
 
       for (Map.Entry node : nextNodes.entrySet()) {
+
+
           State nextNode = (State) node.getKey();
-          if (evaluateState(nextNode) >= evaluateState(bestNode)) {
+//          System.out.println("Último movimento: " + lastMove);
+//          System.out.println(nextNodes.get(nextNode));
+//          System.out.println(" ");
+
+          double turnaroundPenalty = (lastMove == nextNodes.get(nextNode).getOpposite() ? -10.0 : 0.0);
+
+          if (evaluateState(nextNode) + turnaroundPenalty >= evaluateState(bestNode)) {  //Isso evita que o PacMan faca o mesmo movimento anterior sem considerar danos graves
               bestNode = nextNode;
               bestMove = nextNodes.get(bestNode);
           }
       }
+      System.out.println("Movimento escolhido: " + bestMove);
+      System.out.println(" ");
 
+      lastMove = bestMove;
       return bestMove;
   }
 
@@ -54,7 +84,12 @@ public class GreedyPacManPlayer implements PacManPlayer, StateEvaluator {
    * @return an estimate of the value of the State.
    */
   public double evaluateState(State state) {
-     double heuristic = 1;
+      Location pacLOC = state.getPacManLocation();
+      List<Location> allGhostLoc = state.getGhostLocations();
+      LocationSet allDotLoc = state.getDotLocations();
+
+
+      double heuristic = 1;
 
     //TESTE DE FUNCAO OBJETIVO
     if (Game.isLosing(state))
@@ -69,23 +104,19 @@ public class GreedyPacManPlayer implements PacManPlayer, StateEvaluator {
 
 //
 //    //HEURÍSTICA 1: DISTÂNCIA ENTRE PACMAN E O FANTASMA MAIS PRÓXIMO
-     Location pacLOC = state.getPacManLocation();
-     List<Location> allGhostLoc = state.getGhostLocations();
-
      heuristic += pacLOC.manhattanDistanceToClosest(pacLOC, allGhostLoc);
 //    //HEURÍSTICA 1: DISTÂNCIA ENTRE PACMAN E O FANTASMA MAIS PRÓXIMO
 
 
 
       //HEURÍSTICA 2: PONTOS EXISTENTES NO MAPA
-      LocationSet allDotLoc = state.getDotLocations();
       heuristic -= allDotLoc.size();
       //HEURÍSTICA 2: PONTOS EXISTENTES NO MAPA
 
 
-      //HEURÍSTICA 3: DISTÂNCIA ENTRE O PAMAN E O PONTO MAIS PRÓXIMO
-//      heuristic -= pacLOC.manhattanDistanceToClosest(pacLOC, allDotLoc);
-      //HEURÍSTICA 3: DISTÂNCIA ENTRE O PAMAN E O PONTO MAIS PRÓXIMO
+      //HEURÍSTICA 3: DISTÂNCIA ENTRE O PACMAN E O PONTO MAIS PRÓXIMO
+      heuristic -= pacLOC.manhattanDistanceToClosest(pacLOC, allDotLoc);
+      //HEURÍSTICA 3: DISTÂNCIA ENTRE O PACMAN E O PONTO MAIS PRÓXIMO
 
 
 
@@ -94,6 +125,5 @@ public class GreedyPacManPlayer implements PacManPlayer, StateEvaluator {
   }
 
 
-  private float
 
 }
